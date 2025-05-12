6a838b3c0145c1dcb06430537821cace270900ef
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-05-12 17:43:49 +0530

    variables in go

--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"mylearning/myutils"
+)
 
 func main() {
 	fmt.Println("jai baaba ki ")
+
+	var name string = "aashu "
+	fmt.Println((name))
+
+	var version = 1.0
+	fmt.Println(version)
+
+	var monney int = 50000
+	fmt.Println(monney)
+
+	var dimension float64 = 345.12
+	fmt.Println(dimension)
+
+	var decided bool = false
+	decided = true
+	fmt.Println(decided)
+
+	const pi = 67.12
+	fmt.Println(pi)
+
+	// special type of varibale declarelation
+
+	person := 123
+	fmt.Println(person)
+
+	// impoeting and exporting by capital letter
+	// var Public = "data is impprtant "; // here the letter is capital
+	var private = "data is private "
+
+	fmt.Println((myutils.Public))
+	fmt.Println((private))
+
 }
---
+++ myutils/myutils.go
@@ -0,0 +1,9 @@
+package myutils
+
+import "fmt"
+
+var Public = "data is impprtant " // here the letter is capital
+
+func Message(data string) {
+	fmt.Println(data)
+}
