e81e69c43b78ab8deab77362cd585318d1d6a400
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-05-31 23:41:33 +0530

    added my message

--- myutils/myutils.go
+++ myutils/myutils.go
@@ -7,3 +7,5 @@ var Public = "data is impprtant " // here the letter is capital
 func Message(data string) {
 	fmt.Println(data)
 }
+
+// I am definetly going to become financially free and a world travel
